b06feeb658f52cc070d06009cc092622eef8deea
rmateixeira94 <rmateixeira94@gmail.com>
2025-03-19 22:36:32 +0000

    Add files via upload

---
+++ data.js
@@ -0,0 +1,88 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_3468",
+      "name": "IMG_3468",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -1.6847233523543146,
+        "pitch": 0.2696874261720481,
+        "fov": 1.5552936603672898
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4427670123762901,
+          "pitch": 0.2208895047508488,
+          "rotation": 0,
+          "target": "1-img_3469"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-img_3469",
+      "name": "IMG_3469",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
